b5795e91d69207dcaef03d55e3c845d7b661d3db
lucasmiranda <93141172+mucasliranda@users.noreply.github.com>
2023-10-22 11:56:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hobbie/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.hobbie.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Primary = Color(0xFFF1E8DA)
+
+val Secondary = Color(0xFFFF7930)
+
+val Black = Color(0xFF000000)
+
+val White = Color(0xFFFFFFFF)
+
+
+
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
