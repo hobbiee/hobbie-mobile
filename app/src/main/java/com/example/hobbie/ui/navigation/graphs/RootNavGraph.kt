e9497abfd652a695a4979094872d6e8121de5095
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-28 01:44:02 -0300

    feat(`RootNavGraph`): Add sign-up and sign-in screens to the authentication navigation
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- app/src/main/java/com/example/hobbie/ui/navigation/graphs/RootNavGraph.kt
+++ app/src/main/java/com/example/hobbie/ui/navigation/graphs/RootNavGraph.kt
@@ -5,24 +5,33 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import com.example.hobbie.ui.navigation.layout.HomeLayout
-import com.example.hobbie.ui.screens.login.LoginScreen
+import com.example.hobbie.ui.screens.authentication.login.LoginScreen
+import com.example.hobbie.ui.screens.authentication.register.SignUpScreen
 
 @Composable
 fun RootNavGraph(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
-        startDestination = Graph.HOME,
+        startDestination = Graph.AUTHENTICATION,
         route = Graph.ROOT
     ) {
-        // AQUI VAI ENTRAR UMA NESTED NAVIGATION P CRIAR USUÁRIO E TODA AS FIRULAS DELE
-        // MAS, UM PROBLEMA DE CADA VEZ
-        composable(
-            route = Graph.AUTHENTICATION
+        navigation(
+            startDestination = Graph.SIGN_UP,
+            route = Graph.AUTHENTICATION,
         ) {
-            LoginScreen(
-                navController = navController
-            )
+            composable(
+                route = Graph.SIGN_UP
+            ) {
+                SignUpScreen(navController = navController)
+            }
+
+            composable(
+                route = Graph.SIGN_IN
+            ) {
+                LoginScreen(navController = navController)
+            }
         }
 
         composable(
@@ -36,6 +45,8 @@ fun RootNavGraph(navController: NavHostController = rememberNavController()) {
 object Graph {
     const val ROOT = "root"
     const val AUTHENTICATION = "auth"
+    const val SIGN_IN = "sign_in"
+    const val SIGN_UP = "sign_up"
     const val HOME = "home"
     const val EVENTS = "events"
 }
