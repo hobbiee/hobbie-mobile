2cc01775d263b5b5469459c5f31647f8589c653a
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-29 11:02:38 -0300

    Define POST endpoint to allow username-password login
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- app/src/main/java/com/example/hobbie/api/HobbieAPI.kt
+++ app/src/main/java/com/example/hobbie/api/HobbieAPI.kt
@@ -1,12 +1,21 @@
 package com.example.hobbie.api
 
 import com.example.hobbie.models.Event
+import com.example.hobbie.models.http.LoginRequest
+import com.example.hobbie.models.http.LoginResponse
+import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface HobbieAPI {
 
     @GET("/v1/api/events/2")
     suspend fun getPlayerEvents(): Response<Event>
 
+    @Headers("Content-Type: application/json")
+    @POST("/v1/api/auth/username-password")
+    fun doLogin(@Body loginRequest: LoginRequest): Call<LoginResponse>
 }
